084527603f5eab8d031c2a69e7f54dc51909b613
marina <marinacbraga@hotmail.com>
2023-10-20 22:29:36 -0300

    nova conexao com banco + att para deploy

--- views/inicio/script.js
+++
@@ -1,8 +0,0 @@
-function selectBtn(id) {
-    if (document.getElementById(id).classList.contains("selected")) {
-        document.getElementById(id).classList.remove("selected");
-    }
-    else {
-        document.getElementById(id).classList.add("selected");
-    }
-}
